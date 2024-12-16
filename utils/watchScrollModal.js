944045e770255acced1cdaea29e5da27cd4848ec
Edgar <edgar.torosyan1919@gmail.com>
2024-12-16 17:35:25 +0400

    Change scroll-fixes watcher functionality

--- utils/watchScrollModal.js
+++ utils/watchScrollModal.js
@@ -1,15 +1,21 @@
 function watchScrollModal(isOpen) {
+  console.log(isOpen)
+  
+  if (window.fullpage_api) {
     if (isOpen) {
-      if (window.fullpage_api) {
-        window.fullpage_api.setMouseWheelScrolling(false)
-        window.fullpage_api.setAllowScrolling(false)
-      }
+      window.fullpage_api.setMouseWheelScrolling(false)
+      window.fullpage_api.setAllowScrolling(false)
     } else {
-      if (window.fullpage_api) {
-        window.fullpage_api.setMouseWheelScrolling(true)
-        window.fullpage_api.setAllowScrolling(true)
-      }
+      window.fullpage_api.setMouseWheelScrolling(true)
+      window.fullpage_api.setAllowScrolling(true)
     }
+  } else {
+    if (isOpen) {
+      document.body.style.overflow = 'hidden'
+    } else {
+      document.body.style.overflow = 'unset'
+    }
+  }
 }
 
 export default watchScrollModal
